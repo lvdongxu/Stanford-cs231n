cc307779789506d4540d2a4b8b48624ea5685003
lvdongxu <36668682+lvdongxu@users.noreply.github.com>
2019-02-02 16:58:58 +0800

    Add files via upload

---
+++ go.sh
@@ -0,0 +1,6 @@
+echo "# Stanford-cs231n" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/lvdongxu/Stanford-cs231n.git
+git push -u origin master
